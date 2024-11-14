a5808a039dc72a4bde8bce08e4c6aa0ce722480e
helloAmulya <internatomix@gmail.com>
2024-11-14 06:51:07 +0530

    add movie page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,7 +9,7 @@ export default function Card({ result }) {
       className="group cursor-pointer sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2
     transition-shadow duration-200"
     >
-      <Link href={`/movies/${result.id}`}>
+      <Link href={`/movie/${result.id}`}>
         <Image
           src={`https://image.tmdb.org/t/p/original/${
             result.backdrop_path || result.poster_path
