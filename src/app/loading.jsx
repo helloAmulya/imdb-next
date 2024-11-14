38eeae0adbd260cfc5a90554dff57d64ff0e37dc
helloAmulya <internatomix@gmail.com>
2024-11-15 00:02:00 +0530

    Done

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      {/* <img className="h-52" src="spinner.svg" alt="loading" /> */}
-      <Image className="h-52" src="spinner.svg" alt="loading" />
+      <img className="h-52" src="spinner.svg" alt="loading" />
+      {/* <Image className="h-52" src="spinner.svg" alt="loading" /> */}
     </div>
   );
 }
