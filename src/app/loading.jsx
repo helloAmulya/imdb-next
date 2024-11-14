9d04a8b8725760c5e3b63db055af9fb82786b1c4
helloAmulya <internatomix@gmail.com>
2024-11-14 23:59:06 +0530

    loading file upadate

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Image from "next/image";
 
 export default function loading() {
   return (
     <div className="flex justify-center mt-16">
-      <img className="h-52" src="spinner.svg" alt="loading" />
+      {/* <img className="h-52" src="spinner.svg" alt="loading" /> */}
+      <Image className="h-52" src="spinner.svg" alt="loading" />
     </div>
   );
 }
